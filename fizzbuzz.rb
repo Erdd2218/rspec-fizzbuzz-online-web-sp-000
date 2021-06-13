fa512b609b030c8c9eac250134612914f7c2b99f
Andre Pierre <andre.laurence94@gmail.com>
2021-06-13 20:13:56 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,13 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(num)
-  if num % 3 == 0 
+  if num % 3 ==  0 
     puts "Fizz"
-  end
-end 
+    elsif num % 5 == 0 
+    puts "Buzz"
+    elsif num % 3 == 0 && num % 5 == 0 
+    puts "FizzBuzz"
+  else 
+    puts nil 
+  end 
+end
